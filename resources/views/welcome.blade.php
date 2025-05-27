3cede610d45ce2e9222e7fa27893d606629e6451
JannatulFerdousRiva <riva15-4928@diu.edu.bd>
2025-05-27 17:29:44 +0600

    Protfolio 1st design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,38 @@
     <title>Document</title>
 </head>
 <body>
-    <p> Hi,I am Riva. </p>
-    <h1>First Laravel</h1>
+    <div>
+        <div>
+            <h3>Jannatul Ferdous Riva</h3>
+            <h3>Education</h3>
+            <h3>Skills</h3>
+            <h3>Project</h3>
+        </div>
+        <div>
+            <div>
+                <h3>About Me</h3>
+            </div>
+            <div>
+                <h3>Picture</h3>
+        </div>
+        <div>
+            <h3>Skills</h3>
+        </div>
+        <div>
+            <h3>
+                Contact Details
+            </h3>
+        </div>
+        <div>
+            <h3>
+                Social Media
+            </h3>
+        </div>
+        <div>
+            <h3>
+                Footer
+            </h3>
+        </div>
+    </div>
 </body>
 </html>
